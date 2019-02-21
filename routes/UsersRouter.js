7c5d9497a2f48cbf712aba053c6aa52d38cc8574
A.Kholaif <ahmed_kholaif@yahoo.com>
2019-02-21 11:16:49 +0200

    register ,tokens

--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -1,45 +1,87 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-
-
-router.post('/',(req,res)=>{
+const bcrypt = require('bcryptjs');
+const {authenticate,auth_Admin} = require('../helpers/Auth');
+router.post('/register',(req,res)=>{
     console.log("reached");
 
-    const firstName = req.body.fname;
-    const lastName = req.body.lname;
+    
+    const email = req.body.email;
+ 
+    
+    User.findOne({email})
+    .then(user=>{
+        if(user) {
+            return res.status(400).json({email:"Email already exists "});
+        }else {
+            const firstName = req.body.fname;
+            const lastName = req.body.lname;
+            const password = req.body.password;
+            const imgSrc = req.body.img ;// == image source
+
+            const user = new User({
+                firstName ,
+                lastName,
+                email,
+                password,
+                imgSrc,
+                isAdmin:req.body.isAdmin
+            });
+            
+            user.save()
+            .then ( () =>  {
+                return user.getAuthToken();})
+                .then(token => {
+                    res.header('x-auth',token).send(user);
+                })
+            .catch(e => res.status(404).send(e));
+        }
+    } )
+    
+});
+
+router.post('/login',(req,res)=>{
     const email = req.body.email;
     const password = req.body.password;
-    const imgSrc = req.body.img ;// == image source 
-    
-    const user = new User({
-        firstName ,
-        lastName,
-        email,
-        password,
-        imgSrc,
-    });
-    
-    user.save()
-    .then ( () =>  {
-        return user.getAuthToken();})
-        .then(token => {
-            res.header('x-auth',token).send();
-        })
-    .catch(e => res.status(404).send(e));
-});
 
-router.post('/signin',(req,res)=>{
-    
+    User.findOne({email})
+    .then(user =>{
+        if(!user) {
+            return res.status(404).json({email:"Invalid Email Or Password "})
+        }else {
+            bcrypt.compare(password,user.password)
+            .then(isMatch=>{
+                if(isMatch){
+                    return user.getAuthToken()
+                    .then(token => {
+                        res.header('x-auth',token).send(user);
+                    })
+                } else {
+                    return res.status(400).json({password:"Invalid Email Or Password"});
+                }
+            })
+        }
+    })
 
 });
 
-router.get('/id',(req,res)=>{
 
+//  Private route
+//
 
+router.get('/current',authenticate,(req,res)=>{
+    res.send(req.user)
 })
 
+//
 
+router.delete('/current/logout',authenticate,(req,res)=>{
+
+    req.user.removeToken(req.token)
+    .then(()=>res.status(200).send())
+    .catch(()=> res.status(404).send());
+});
 
 
 
