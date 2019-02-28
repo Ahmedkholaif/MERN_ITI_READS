1f5074e80241583b1fc9947c246a219254176cf3
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-02-28 20:59:02 +0200

    signout & redirection

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -4,6 +4,7 @@ import classnames from 'classnames';
 import CategoriesView from '../components/CategoriesView'
 import AuthorView from '../components/AuthorView'
 import BookView from '../components/BookView'
+import axios from 'axios';
 
 import '../css/AdminLogin.css';
 
@@ -21,10 +22,20 @@ export default class Example extends React.Component {
         };
     }
 
-    handleSignout() {
-        // handle using redirect
-        // handle with login
-        // handle using localStorage
+    handleSignout(event) {
+        event.preventDefault();
+        if(localStorage.token){
+            const conf= {
+                headers: {'x-auth':token}
+            };
+            axios.delete('/api/admin/signout',conf)
+                .then(response=>{
+                    localStorage.clear();
+                    window.location.href = '/';
+                }).catch(error=>{
+                    console.log(error);
+            });
+        }
     }
 
     handlePassCategoriesProps(categoriesProps) {
@@ -50,60 +61,60 @@ export default class Example extends React.Component {
     render() {
 
         return (
-        localStorage.token ?
-            <div className='AdminDashboard'>
-                <Nav tabs>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({active: this.state.activeTab === '1'})}
-                            onClick={() => {
-                                this.toggle('1');
-                            }}
-                        >
-                            Categories
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({active: this.state.activeTab === '2'})}
-                            onClick={() => {
-                                this.toggle('2');
-                            }}
-                        >
-                            Books
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({active: this.state.activeTab === '3'})}
-                            onClick={() => {
-                                this.toggle('3');
-                            }}
-                        >
-                            Authors
-                        </NavLink>
-                    </NavItem>
-                    <Button color='danger'>SignOut</Button>
-                </Nav>
-                <TabContent activeTab={this.state.activeTab}>
-                    <TabPane tabId="1">
-                        <Col sm="12">
-                            <CategoriesView passCategories={this.handlePassCategoriesProps}/>
-                        </Col>
-                    </TabPane>
-                    <TabPane tabId="2">
-                        <Col sm="12">
-                            <BookView categories={this.state.categories} authors={this.state.authors}/>
-                        </Col>
-                    </TabPane>
-                    <TabPane tabId="3">
-                        <Col sm="12">
-                            <AuthorView passAuthors={this.handlePassAuthorsProps} />
-                        </Col>
-                    </TabPane>
-                </TabContent>
-            </div>
-            :<Redirect to={{pathname: '/', state: {from: this.props.location}}}/>
+            localStorage.token ?
+                <div className='AdminDashboard'>
+                    <Nav tabs>
+                        <NavItem>
+                            <NavLink
+                                className={classnames({active: this.state.activeTab === '1'})}
+                                onClick={() => {
+                                    this.toggle('1');
+                                }}
+                            >
+                                Categories
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className={classnames({active: this.state.activeTab === '2'})}
+                                onClick={() => {
+                                    this.toggle('2');
+                                }}
+                            >
+                                Books
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className={classnames({active: this.state.activeTab === '3'})}
+                                onClick={() => {
+                                    this.toggle('3');
+                                }}
+                            >
+                                Authors
+                            </NavLink>
+                        </NavItem>
+                        <Button color='danger' onClick={() => this.handleSignout}>SignOut</Button>
+                    </Nav>
+                    <TabContent activeTab={this.state.activeTab}>
+                        <TabPane tabId="1">
+                            <Col sm="12">
+                                <CategoriesView passCategories={this.handlePassCategoriesProps}/>
+                            </Col>
+                        </TabPane>
+                        <TabPane tabId="2">
+                            <Col sm="12">
+                                <BookView categories={this.state.categories} authors={this.state.authors}/>
+                            </Col>
+                        </TabPane>
+                        <TabPane tabId="3">
+                            <Col sm="12">
+                                <AuthorView passAuthors={this.handlePassAuthorsProps}/>
+                            </Col>
+                        </TabPane>
+                    </TabContent>
+                </div>
+                : <Redirect to={{pathname: '/', state: {from: this.props.location}}}/>
         );
     }
 }
